89ba0770029f729eb800ed6261845d080d491f81
Nika Salia <82905597+anosckin@users.noreply.github.com>
2025-07-31 17:51:17 +0100

    [State] Update isExpanded on non truncated strings (#1)
    Summary:
    - Fix demo app
    - Fix init comments
    - Set isExpanded to true, if the string does not need truncation.
    
    Test Plan:
    Build & make sure default expandion logic works in demo app

--- Example/ExpandableTextExample/ContentView.swift
+++ Example/ExpandableTextExample/ContentView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 import ExpandableText
 
 struct ContentView: View {
+    @State private var isExpanded1 = false
+    @State private var isExpanded2 = false
+    @State private var isExpanded3 = false
+    @State private var isExpanded4 = false
+    @State private var isExpanded5 = false
     
     let loremIpsum = #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."#
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 20) {
-                ExpandableText(loremIpsum)
+                ExpandableText(loremIpsum, expanded: $isExpanded1)
                     .border(.red)
                 
-                ExpandableText(loremIpsum)
+                ExpandableText(loremIpsum, expanded: $isExpanded2)
                     .border(.red)
                     .environment(\.layoutDirection, .rightToLeft)
                 
-                ExpandableText(loremIpsum)
+                ExpandableText(loremIpsum, expanded: $isExpanded3)
                     .font(.system(.headline, design: .rounded))
                     .foregroundColor(.secondary)
                     .lineLimit(4)
@@ -34,10 +39,10 @@ struct ContentView: View {
                     .enableCollapse(true)
                     .border(.red)
                 
-                ExpandableText("**Markdown** is _supported_")
+                ExpandableText("**Markdown** is _supported_", expanded: $isExpanded4)
                     .border(.red)
                 
-                ExpandableText(String(localized: "EXAMPLE_LOCALIZED_TEXT_KEY"))
+                ExpandableText(String(localized: "EXAMPLE_LOCALIZED_TEXT_KEY"), expanded: $isExpanded5)
                     .border(.red)
                 
                 Spacer()
